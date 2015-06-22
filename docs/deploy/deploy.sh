32728747da4b530c777bd7c499ac4ea84f7535b0
Eduardo Rodrigues <eduardor@lt-mbp-sp-015.ny.rga.com>
2015-06-22 15:34:47 -0300

    updating deploy file

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-export RAILS_ENV=production
 cd /home/deploy/discourse-dev.rgadev.com/discourse
+bluepill stop
+bluepill quit
 git pull
-bundle update
-bundle exec rake assets:precompile
+bundle install --without test --deployment
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake db:migrate
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake assets:precompile
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ROOT=/home/deploy/discourse-dev.rgadev.com/discourse RAILS_ENV=production NUM_WEBS=2 /home/deploy/.rvm/bin/bootup_bluepill --no-privileged -c ~/.bluepill load /home/deploy/discourse-dev.rgadev.com/discourse/config/discourse.pill
 sudo service nginx restart
