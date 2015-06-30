5b28b3bf99f2c7bd93a13bbc3a78909c1352065b
Eduardo Rodrigues <eduardor@lt-mbp-sp-015.ny.rga.com>
2015-06-30 17:16:16 -0300

    trying to fix login callback cors problem

---
+++ plugins/google-auth-custom-callback/plugin.rb
@@ -0,0 +1,38 @@
+# name: google-auth-custom-callback
+# about: 
+# version: 0.1
+# authors: 
+# url: 
+
+PLUGIN_NAME ||= "google_auth_custom_callback".freeze
+
+
+after_initialize do
+
+
+  module ::GoogleAuthCustomCallback
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace GoogleAuthCustomCallback
+    end
+  end
+
+  Users::OmniauthCallbacksController.class_eval do
+    def complete
+      auth = request.env["omniauth.auth"]
+      auth[:session] = session
+
+      authenticator = self.class.find_authenticator(params[:provider])
+
+      @data = authenticator.after_authenticate(auth)
+      @data.authenticator_name = authenticator.name
+
+      complete_response_data
+
+      render "/plugins/google-auth-custom-callback/app/views/google_auth_custom_callback/complete"
+    end
+  end
+
+
+  
+end
